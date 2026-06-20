75397c1c1a7ddeed253d127de552599fd8663bc1
U Naveen Babu <136116172+naveen0913@users.noreply.github.com>
2026-06-20 16:12:40 +0530

    updated inject

--- src/app/components/cta-banner/cta-banner.component.ts
+++ src/app/components/cta-banner/cta-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
